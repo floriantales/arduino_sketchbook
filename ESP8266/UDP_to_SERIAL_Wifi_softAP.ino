332c15dbd36d8c52596139e545af5f6e5259883e
floriantales <florian.tales@laposte.net>
2018-01-24 21:37:55 +0100

    Update UDP_to_SERIAL_Wifi_softAP.ino

--- ESP8266/UDP_to_SERIAL_Wifi_softAP.ino
+++ ESP8266/UDP_to_SERIAL_Wifi_softAP.ino
@@ -6,7 +6,7 @@ IPAddress local_IP(10,10,10,1);
 IPAddress gateway(10,10,10,0);
 IPAddress subnet(255,255,255,0);
 const char *ssid = "UDP_to_SERIAL";
-const char *password = "12345678"; // Must be 8 chars min (wpa2) see https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/soft-access-point-class.html#softap
+const char *password = "********"; // Must be 8 chars min (wpa2) see https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/soft-access-point-class.html#softap
 
 // UDP settings
 WiFiUDP Udp;
