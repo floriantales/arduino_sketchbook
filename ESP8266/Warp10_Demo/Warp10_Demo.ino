ac64c889a7bd8c69961b51c0192093a6358a751e
Florian TALES <florian.tales@laposte.net>
2020-10-20 11:36:54 +0200

    Warp10_Demo : gestion des cas null : lat/lon elev

--- ESP8266/Warp10_Demo/Warp10_Demo.ino
+++ ESP8266/Warp10_Demo/Warp10_Demo.ino
@@ -4,30 +4,24 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssid = "*****";
-const char* password = "*****";
+// Wifi config
+#define WIFI_HOSTNAME "Warp10_Demo"
+const char* ssid = "******";
+const char* password = "******";
 
+// Warp10 config
 #define WARP10_SERVER_IP "192.168.0.11"
 #define WARP10_SERVER_PORT 80
 #define WARP10_UPDATE_URL "/api/v0/update"
 #define WARP10_WRITE_TOKEN "AP_8QdbvhyjFJuuOoohNyHJClJd7ODr.vP5GMt.Y6irthsyFdeaZt_vx2CeCrQfpF465ADT1RKD5e488pteN2MhfVomQbEHAPX8Ra3foeYo"
 #define WARP10_CLASS_NAME "GTS_demo"
 
-// Pour la demo
-unsigned long gts_timestamp_s = 0; // seconds -> zero will let warp10 server fixe the timestamp for you
-String gts_timestamp_us = "";    // nothing to send to warp10
-String gts_gps_lat = "48.44484"; // A voir pour passer en  double 64 bits
-String gts_gps_lon = "-4.46653"; // A voir pour passer en  double 64 bits
-long gts_gps_elev_m = 150;  // meters
-String gts_labels = "lieu=maison";
-int  gts_value = 0;
-String gts_post_string = "";
-
 void setup()
 { 
   Serial.begin(115200);
   Serial.println();
- 
+
+  WiFi.hostname(WIFI_HOSTNAME);
   Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
@@ -42,15 +36,40 @@ void setup()
 
 void loop()
 {
-  // Ecrriture des donnees
+  // ALIMENTATION GTS
+  unsigned long gts_timestamp_s = 0;  // epoch in seconds -> zero will let warp10 server fixe the timestamp for you
+  String gts_gps_lat = "48.44484";    // A voir pour passer en  double 64 bits // xx let warp10 server fixe no LAT/LON values
+  String gts_gps_lon = "-4.46653";    // A voir pour passer en  double 64 bits
+  int gts_gps_elev_m = -1;            // meters -> -1 will let warp10 server fixe no elevation value
+  String gts_labels = "id=wemos1";
+  float  gts_value = 0;
+
+  // FORMATTAGE STRING POST
+  String gts_post_string = "";
+  // Timestamp
+  String gts_timestamp_us = "";    // default : nothing to send to warp10
   if (gts_timestamp_s != 0) // GTS Timestamp
   {
-    String gts_timestamp_us = String(gts_timestamp_s) + "000000";
+    gts_timestamp_us = String(gts_timestamp_s) + "000000";
   }
-  gts_value = gts_value + 1;  //  GTS Value
-  gts_post_string = gts_timestamp_us + "/" + gts_gps_lat + ":" + gts_gps_lon + "/" + String(gts_gps_elev_m) + "000 " WARP10_CLASS_NAME "{" + gts_labels + "} " + String(gts_value); //  GTS Post String
+  // GPS Elevation
+  String gts_gps_elev_mm = "";    // default : nothing to send to warp10
+  if (gts_gps_elev_m != -1) // GTS Timestamp
+  {
+    gts_gps_elev_mm = String(gts_gps_elev_m) + "000";
+  }
+  // GPS lat/lon
+  String gts_gps_coordinates = "";    // default : nothing to send to warp10
+  if (gts_gps_lat != "xx") // GTS Timestamp
+  {
+    gts_gps_coordinates = gts_gps_lat + ":" + gts_gps_lon;
+  }
+  // Write POST string
+  gts_post_string = gts_timestamp_us + "/" + gts_gps_coordinates + "/" + gts_gps_elev_mm + " " + WARP10_CLASS_NAME "{" + gts_labels + "} " + String(gts_value); //  GTS Post String
+  Serial.println("    data    = " + gts_post_string);
+
   
-  // Test de connectivite
+  // WARP10 SERVER CONNECTIVITY TEST
   Serial.print("Connectivity test : ");
   WiFiClient client; // Creation object client
   if (!client.connect(WARP10_SERVER_IP, WARP10_SERVER_PORT))  // On test
@@ -62,7 +81,8 @@ void loop()
   Serial.println("Pass");
   client.stop();
 
-  // Post HTTP
+
+  // HTTP POST to WARP10 SERVER
   HTTPClient http;                                                                                  // Create http object
   Serial.println("POST GTS to warp10 : ");
   Serial.println("    url     = http://" WARP10_SERVER_IP ":" + String(WARP10_SERVER_PORT) + WARP10_UPDATE_URL);
@@ -85,7 +105,8 @@ void loop()
   }
   http.end(); // Close connection
 
-  // On glande (Deep Sleep si batterie)
+
+  // WAIT BEFORE RESTART LOOP (Deep Sleep si batterie)
   Serial.println("Waiting ...");
   delay(5000);
 }
